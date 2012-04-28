6f8445c635c9590f080f29e72332cbaf2bec6f1d
Martin O'Leary <m.e.w.oleary@gmail.com>
2012-04-28 17:45:23 -0400

    Optimized prediction script.

--- predict_colisten.py
+++ predict_colisten.py
@@ -11,19 +11,32 @@ mtxfile, evalfile, outfile = sys.argv[1:]
 colisten = scipy.io.mmread(file(mtxfile)).tocsr()
 listens = colisten.diagonal()
 
+listenranked = numpy.argsort(-listens)[:500]
+
 with open(outfile, 'w') as out:
   for history in util.songs_by_user(evalfile):
     songs, counts = zip(*history)
-    songs = numpy.array(songs)
     
-    sim = numpy.ravel(numpy.sum(counts * colisten[songs - 1,:].todense(), 0))
-    ranked = numpy.flipud(numpy.lexsort((listens, sim)) + 1)
+    sim = numpy.array(counts)[numpy.newaxis, :] * colisten[numpy.array(songs) - 1,:]
+        
+    # All this nonsense is an optimization to avoid the fact that
+    # sorting 300,000 numbers 110,000 times is bad for your health.
+    # I only sort the songs where sim > 0
+    simidxs = sim.nonzero()[1]
+    srt = numpy.lexsort((-listens[simidxs], -sim[0,simidxs]))
+    rankidxs = simidxs[srt]
     
     guess = []
-    for s in ranked:
-      if s in songs:
+    for s in rankidxs:
+      if s+1 in songs:
         continue
-      guess.append(str(s))
+      guess.append(str(s+1))
       if len(guess) == 500: break
+    else:
+      for s in listenranked:
+        if s+1 in songs or s in rankidxs:
+          continue
+        guess.append(str(s+1))
+        if len(guess) == 500: break
       
     out.write(' '.join(guess) + '\n')
